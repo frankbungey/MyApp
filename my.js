3cd8f57b0e0e1a8f8b0be38e625765ad8668c513
frankbungey <frankbungey@gmail.com>
2023-05-13 10:41:06 +0300

    MyApp ,Record keeper

---
+++ my.js
@@ -0,0 +1,21 @@
+
+
+let saveEl= document.getElementById("save-el")
+let countEl = document.getElementById("count-el")
+console.log(countEl)
+
+let count = 0
+function increment () {
+
+    count = count + 1
+    countEl.innerText= count
+    console.log(count)
+}
+
+function save() {
+   let countstr = count + " - "
+   saveEl.innerText += countstr
+   countEl.innerText= 0
+   count=0
+
+} 
